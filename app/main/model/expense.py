fd2c47b6eef579cb1155db2bbfa436cebaf4567f
Daxxxan <alex-bad97@hotmail.fr>
2020-06-12 23:02:24 +0200

    modify cost type

--- app/main/model/expense.py
+++ app/main/model/expense.py
@@ -4,6 +4,6 @@ from .. import db
 class Expense(db.Model):
     __tablename__ = "expense"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cost = db.Column(db.Integer, unique=False)
+    cost = db.Column(db.Float, unique=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     trip_id = db.Column(db.Integer, db.ForeignKey('trip.id'))
--- app/main/model/owe.py
+++ app/main/model/owe.py
@@ -4,6 +4,6 @@ from .. import db
 class Owe(db.Model):
     __tablename__ = "owe"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cost = db.Column(db.Integer, unique=False)
+    cost = db.Column(db.Float, unique=False)
     expense_id = db.Column(db.Integer, db.ForeignKey('expense.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
